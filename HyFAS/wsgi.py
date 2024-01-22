c4be1f5fa46b90a25d6e58511d2a1ec75659dc23
garlitjames <garlitjamessevilla@gmail.com>
2024-01-23 04:43:36 +0800

    add some file for deployment

--- HyFAS/wsgi.py
+++ HyFAS/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HyFAS.settings')
+settings_module = 'HyFAS.deplyoment' if 'WEBSITE_HOSTNAME' in os.environ else 'HyFAS.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HyFAS.settings')
 
 application = get_wsgi_application()
