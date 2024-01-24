bc6c3344886da518678fb4b09bdf25654a4e033b
garlitjames <garlitjamessevilla@gmail.com>
2024-01-25 03:04:13 +0800

    up

--- HyFAS/settings.py
+++ HyFAS/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!8s#p+l^wi3b)0c7vvd%nrcbp5o^ilof79fif-_mc^8ob(71m@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True')=="True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "https://hfam.onrender.com"]
 
 
 # Application definition
@@ -77,16 +78,23 @@ WSGI_APPLICATION = 'HyFAS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hyfas',
-        'USER': 'postgres',
-        'PASSWORD':'attendancesys',
-        'HOST':'localhost',
-        'PORT':'5432',
+if not DEBUG:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}  
+
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'hyfas',
+            'USER': 'postgres',
+            'PASSWORD':'attendancesys',
+            'HOST':'localhost',
+            'PORT':'5432',
+        }
+    }  
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
