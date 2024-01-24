e9a212b508ae0b21c230b20d55b01bfc9e468018
garlitjames <garlitjamessevilla@gmail.com>
2024-01-25 02:38:37 +0800

    update

--- HyFAS/settings.py
+++ HyFAS/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'hyfas',
         'USER': 'postgres',
         'PASSWORD':'attendancesys',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'5432',
     }
 }  
